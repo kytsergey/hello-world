4d87c39c9b0f6fcafd87ed05f151eb0cd3f19fbd
Sergey Kutsenko <35540778+kytsergey@users.noreply.github.com>
2018-01-21 20:26:01 +0200

    Create search.js

---
+++ search.js
@@ -0,0 +1,11 @@
+require (
+string;
+function SearchFilesName (file)
+{
+      if (file == files)
+      {    
+          consule.log( " File Names :" +file);
+          return file
+      }
+ }     
+      varchar file = file.toString("file");     
